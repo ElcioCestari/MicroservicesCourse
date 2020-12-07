7525eadf744bd6c8225e0c6e8437f38c9f205afa
Elcio Cestari Taira <elciotaira@gmail.com>
2020-12-07 18:57:36 -0400

    hr-worker as config server client

--- hr-worker/src/main/java/com/elciocestari/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/elciocestari/hrworker/resources/WorkerResource.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.elciocestari.hrworker.entities.Worker;
 import com.elciocestari.hrworker.repositories.WorkerRepository;
 
@@ -20,6 +22,9 @@ public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+	@Value("${test.config}")
+	private String config;
+	
 	@Autowired
 	private Environment environment;
 
@@ -31,6 +36,14 @@ public class WorkerResource {
 		List<Worker> workers = repository.findAll();
 
 		return ResponseEntity.ok().body(workers);
+	}
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfig() {
+		
+		logger.info("CONFIG: " + config);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping(value = "/{id}")
